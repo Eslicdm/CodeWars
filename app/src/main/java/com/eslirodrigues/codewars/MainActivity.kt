29a61cbfa9b2828ccc77aa291228112570a02500
Eslicdm <eslicdm1@gmail.com>
2022-02-12 08:14:42 -0300

    convertRomanToDecimal

--- app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodeWarsTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Answer(volumeTV().toString())
+                    Answer(convertRomanToDecimal().toString())
                 }
             }
         }
@@ -28,6 +28,53 @@ class MainActivity : ComponentActivity() {
 fun Answer(answer: String) {
     Text(text = answer, fontSize = 25.sp)
 }
+
+// Return a decimal from Roman numeral
+fun convertRomanToDecimal() : Int {
+    val input = "MCMXC"
+
+    var values = 0
+
+    for (value in input) {
+        if (value.toString() == "I") {
+            values += 1
+        }
+        if (value.toString() == "V") {
+            values += 5
+        }
+        if (value.toString() == "X") {
+            values += 10
+        }
+        if (value.toString() == "L") {
+            values += 50
+        }
+        if (value.toString() == "C") {
+            values += 100
+        }
+        if (value.toString() == "D") {
+            values += 500
+        }
+        if (value.toString() == "M") {
+            values += 1000
+        }
+    }
+
+    if (input.contains("IV") || input.contains("IX")) {
+        values -= 2
+    }
+
+    if (input.contains("XL") || input.contains("XC")) {
+        values -= 20
+    }
+
+    if (input.contains("CM") || input.contains("CD")) {
+        values -= 200
+    }
+
+    val answer = values
+    return answer
+}
+
 
 // Max-Vol = 100, Min-Vol = 0. Find the volume
 fun volumeTV() : Int {
