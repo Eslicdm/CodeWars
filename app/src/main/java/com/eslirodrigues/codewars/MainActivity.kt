62da23e7b5fafb91bc8e7a3bd0ac6586a70d8b6f
Eslicdm <eslicdm1@gmail.com>
2022-02-13 12:36:14 -0300

    kidsEnoughComputer

--- app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodeWarsTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Answer(convertRomanToDecimal().toString())
+                    Answer(kidsEnoughComputer().toString())
                 }
             }
         }
@@ -27,6 +27,31 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Answer(answer: String) {
     Text(text = answer, fontSize = 25.sp)
+}
+
+// Find out if have enough pc for students
+fun kidsEnoughComputer() : String {
+    val pcProf = 1
+    val inputAStudents = 3
+    val inputCTotalPc = 6
+    val inputXCaioBurn = 1
+    val inputYNoCompiler = 2
+
+    val workingPcs = inputCTotalPc - pcProf - inputXCaioBurn - inputYNoCompiler
+
+    var answer = ""
+
+    if (workingPcs >= inputAStudents) {
+        answer = "Igor Happy!"
+    }
+    if (workingPcs < inputAStudents) {
+        answer = "Igor Mad!"
+    }
+    if (workingPcs <= inputAStudents && inputXCaioBurn > (inputYNoCompiler / 2)) {
+        answer = "Caio, it's your fault!"
+    }
+
+    return answer
 }
 
 // Return a decimal from Roman numeral
