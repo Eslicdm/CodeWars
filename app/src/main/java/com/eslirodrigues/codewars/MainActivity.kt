b92eab0b12a1ee906a49b8be26ed7d584be1a6b9
Eslicdm <eslicdm1@gmail.com>
2022-03-06 18:09:09 -0300

    howMuchMoneyCrazy

--- app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.eslirodrigues.codewars.ui.theme.CodeWarsTheme
+import kotlinx.coroutines.channels.ticker
 import java.text.DecimalFormat
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodeWarsTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Answer(multiplicationTable().contentDeepToString())
+                    Answer(howMuchMoneyCrazy())
                 }
             }
         }
@@ -27,6 +29,56 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Answer(answer: String) {
     Text(text = answer, fontSize = 25.sp)
+}
+
+// Find out how much money he has
+fun howMuchMoneyCrazy(): String {
+    val start = 1
+    val end = 100 + 1
+
+    val listMoney = mutableListOf(1)
+
+    for (values in start until end) {
+        for (i in start until end) {
+            if (values - 7 * i == 2) {
+                listMoney.add(values)
+            }
+            if (values - 9 * i == 1) {
+                listMoney.add(values)
+            }
+        }
+    }
+
+    val news = listMoney.groupBy { it }.filter { it.value.size > 1 }.keys
+
+    val listBoats = Array(news.size) { it }
+    val listCars = Array(news.size) { it }
+
+    var boatIndex = 0
+    var carIndex = 0
+
+    for (values in start until end) {
+        for (i in news) {
+            if (i - 7 * values == 2) {
+                listBoats[boatIndex] = values
+                boatIndex++
+            }
+            if (i - 9 * values == 1) {
+                listCars[carIndex] = values
+                carIndex++
+            }
+
+        }
+    }
+
+    val array1 = Array(news.size) { index ->
+        arrayOf("M: ${listMoney.groupBy { it }.filter { it.value.size > 1 }.keys.elementAtOrNull(index)} B: ${listBoats[index]} C: ${listCars[index]}")
+    }
+
+    val string: String = array1.contentDeepToString()
+
+    return string.replace(", ", "")
+
 }
 
 // Return an array with multiples
