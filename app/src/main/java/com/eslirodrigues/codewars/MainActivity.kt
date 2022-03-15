0d47d67a5b21c002b231e8b747d44f439c8bad6f
Eslicdm <eslicdm1@gmail.com>
2022-03-15 11:06:36 -0300

    convertDecRgbToHex

--- app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/codewars/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodeWarsTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Answer(howMuchMoneyCrazy())
+                    Answer(convertDecRgbToHex())
                 }
             }
         }
@@ -29,6 +29,44 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Answer(answer: String) {
     Text(text = answer, fontSize = 25.sp)
+}
+
+
+// Convert RGB (decimal) to Hexadecimal
+fun convertDecRgbToHex() : String {
+    val r = 255
+    val g = 255
+    val b = 300
+
+    fun convertDecToHex(number: Int) : String {
+        val remainderList = mutableListOf("")
+
+        var value = if (number > 255) 255 else number
+        if (value == 0) remainderList.add("00")
+
+        while (value != 0) {
+            when (val remainderDiv = value % 16) {
+                10 -> remainderList.add("A")
+                11 -> remainderList.add("B")
+                12 -> remainderList.add("C")
+                13 -> remainderList.add("D")
+                14 -> remainderList.add("E")
+                15 -> remainderList.add("F")
+                else -> remainderList.add(remainderDiv.toString())
+            }
+            value /= 16
+        }
+
+        return remainderList.joinToString(separator = "").reversed()
+    }
+
+    val remainder = mutableListOf(
+        convertDecToHex(r),
+        convertDecToHex(g),
+        convertDecToHex(b)
+    )
+
+    return remainder.joinToString(separator = "")
 }
 
 // Find out how much money he has
